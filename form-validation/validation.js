56004fa8d26fa37dc5e7947f2153aa359cab62d2
Daniel Winters <nosuchlogic@gmail.com>
2022-05-04 22:04:43 +1000

    Form Validation: Finished Styling
    Changed to a more traditional form and styled it how I liked. Finished implementing error message styling and an alert for when the form is successfully validated.

--- form-validation/validation.js
+++ form-validation/validation.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', e => {
 });
 
 function checkValid() {
-	// trim to remove the whitespaces
+  let isValid = 0;
 	const emailValue = email.value.trim();
   const countryValue = country.value.trim();
   const zipValue = zip.value.trim();
@@ -25,25 +25,25 @@ function checkValid() {
 	} else if (!isEmail(emailValue)) {
 		isError(email, 'Please enter a valid email.');
 	} else {
-		noError(email);
+    isValid++;
 	}
 
   if(countryValue === '') {
 		isError(country, 'Please enter the country you live in.');
 	} else {
-		noError(country);
+    isValid++;
 	}
 
   if(zipValue === '') {
 		isError(zip, 'Please enter your zip code.');
 	} else {
-		noError(zip);
+    isValid++;
 	}
 	
 	if(passwordValue === '') {
 		isError(password, 'Please choose a password.');
 	} else {
-		noError(password);
+    isValid++;
 	}
 	
 	if(confValue === '') {
@@ -51,8 +51,15 @@ function checkValid() {
 	} else if(passwordValue !== confValue) {
 		isError(conf, 'Passwords do not match');
 	} else{
-		noError(conf);
+    isValid++;
 	}
+
+  if(isValid == 5) {
+    onSubmit();
+  } else {
+    isValid = 0;
+  }
+
 }
 
 function isError(input, message) {
@@ -61,12 +68,18 @@ function isError(input, message) {
 	formControl.className = 'form-control input-container ic1 error';
 	small.innerText = message;
 }
-
-function noError(input) {
-	const formControl = input.parentElement;
-	formControl.className = 'form-control input-container ic1 success';
-}
 	
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+
+function onSubmit(){
+  console.log('Submission successfully validated.')
+  alert('Submission successfully validated.');
+  email.value = '';
+  country.value = '';
+  zip.value = '';
+  password.value = '';
+  conf.value = '';
+}
+
