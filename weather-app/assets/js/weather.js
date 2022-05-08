9c398ff83d51e09474ae0b68f35f85d5ee5116b3
Daniel Winters <nosuchlogic@gmail.com>
2022-05-08 20:21:33 +1000

    Weather Page: Icons
    Begin implementing icons. Currently grabs icon for top left panel, forecast panel has placeholders.

--- weather-app/assets/js/weather.js
+++ weather-app/assets/js/weather.js
@@ -1,17 +1,20 @@
+doSearch('http://api.openweathermap.org/data/2.5/weather?q=Brisbane&units=metric&appid=97da4e49080576a7d29b10e6a27ba3fa');
+
 // User location search
-async function doSearch(event) {
+function userSearch(event) {
   // Prevent refresh
   event.preventDefault();
   // Grab users search query straight from search box
   const search = document.getElementById('searchBox').value;
-  // If no search was entered on submit, do something
-  if (!search) {
-    console.log('Please enter a city name.');
-    return;
-  }
+
   // Build API call url from user search
   const endpoint = new URL(`http://api.openweathermap.org/data/2.5/weather?q=${ search }&units=metric&appid=97da4e49080576a7d29b10e6a27ba3fa`);
-  // Make API call with the url built
+  doSearch(endpoint);
+}
+
+async function doSearch(endpoint) {
+
+  // Make API call with the url built in userSearch()
   const response = await fetch(endpoint);
   // If call fails, do something
   if (response.status === 404) {
@@ -21,6 +24,7 @@ async function doSearch(event) {
   // Grab the response from the API and make it useable
   const data = await response.json();
   // Update HTML elements with data
+  const currentIcon = document.getElementById('current_icon');
   const temp = document.getElementById('temp');
   const name = document.getElementById('city');
   const weather = document.getElementById('weather');
@@ -28,14 +32,14 @@ async function doSearch(event) {
   const sunset = document.getElementById('sunset');
   const pressure = document.getElementById('pressure');
   const humidity = document.getElementById('humidity');
-  const visibility = document.getElementById('visibility');
   const wind = document.getElementById('wind');
   console.log(data);
   const offset = data.timezone;
   const sunriseTime = data.sys.sunrise;
-  const getSunrise = await getTime(sunriseTime, offset);
+  const getSunrise = await getTime(sunriseTime);
   const sunsetTime = data.sys.sunset;
   const getSunset = await getTime(sunsetTime);
+  currentIcon.src = "./assets/icons/" + data.weather[0].main + ".png";
   temp.innerHTML = parseInt(data.main.temp);
   name.innerHTML = data.name;
   weather.innerHTML = data.weather[0].description;
@@ -43,11 +47,10 @@ async function doSearch(event) {
   sunset.innerHTML = getSunset;
   pressure.innerHTML = data.main.pressure;
   humidity.innerHTML = data.main.humidity;
-  visibility.innerHTML = data.visibility;
   wind.innerHTML = data.wind.speed;
 }
 
-async function getTime(unixtime, timezone) {
+async function getTime(unixtime) {
   const time = new Date(unixtime).toLocaleTimeString(undefined,
     {
       hour: '2-digit',
